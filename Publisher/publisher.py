3b36d982c3dac5a97e6ac07b2121fbafe02b0776
Marcos Carvalho <marcosf40@hotmail.com>
2018-05-25 18:39:27 -0300

    Modularization

--- Publisher/publisher.py
+++ Publisher/publisher.py
@@ -1,36 +1,101 @@
 import logging
 import asyncio
 import random
+import time
 from hbmqtt.client import MQTTClient
 from hbmqtt.mqtt.constants import QOS_1, QOS_2
 
-def generate_data():
-    temperature = [20,21,22,23,27,30,32]
-    choice_temperature = random.choice(temperature)
-    return choice_temperature
-    
+
+class Data(object):
+    """
+            This class is to gerate data.
+            Attributes:
+                    choice_temperature (int): A number to represent the temperature.
+
+     """
+
+    def __init__(self):
+        """ The constructor for Data class."""
+
+        self.choice_temperature = 0
+
+    def set_data(self):
+        """
+                Method for setting a random temperature with the random choice function.
+                Attribute:
+                        temperature (list): An integer list.
+        """
+
+        temperature = [20, 21, 22, 23, 27, 30, 32]  # Vector of temperature
+        self.choice_temperature = random.choice(temperature)
+
+    def get_data(self):
+        """
+                Method to return a value of temperature
+                return:
+                        choice_temperature (int): A value of temperature
+        """
+
+        return self.choice_temperature
 
 
-@asyncio.coroutine
-def send_mqtt():
-    t = generate_data()
-    print("tttt", t)
-    print(type(t))
+class Publisher(object):
+    """
+            This class is to connect and publish values of temperature in MQTT server.
+                    Attribute:
+                            cliente (Object): An object that represent a connection with MQTT
+                            dataToSend (int): An integer representing a temperature.
+                            tasks (generator): An generator representing a task object.
 
-    C = MQTTClient()
-    yield from C.connect('mqtt://192.168.0.16')    
-    tasks = [asyncio.ensure_future(C.publish('a/b', bytes(str(t), 'utf-8')))
-    ]
-    yield from asyncio.wait(tasks)
-    logger.info("messages published")
-    yield from C.disconnect()
+    """
+
+    def __init__(self):
+        """ This constructor for Publisher """
+        self.client = MQTTClient()
+
+    @asyncio.coroutine
+    def send_mqtt(self, data):
+        """
+                        This method is responsible to connect on client	and publish temperature values.
+                        Parameter:
+                                data (int): Value of temperature
+
+        """
+        self.dataToSend = data
+
+        yield from self.client.connect('mqtt://192.168.0.16')
+        tasks = [asyncio.ensure_future(self.client.publish(
+            'sensor/temperature', bytes(str(self.dataToSend), 'utf-8')))]
+        yield from asyncio.wait(tasks)
+
+        # Disconnects the client when finished.
+        yield from self.client.disconnect()
+
+    def run(self, recv_generator):
+        """
+                This method is responsible to start the process.
+                Parameter:
+                        recv_generator (generator): An generator of connection and publish.
+
+        """
+        asyncio.get_event_loop().run_until_complete(decorator)
 
 
+def main():
+	"""
+		This class is responsible to start the program.
+		Attribute:
+			obj_Data (Object): An object of class Data.
+			obj_Publisher (Object): An object of class Publisher
 
+	"""
+    obj_Data = Data()
+    obj_Publisher = Publisher()
+
+    while True:
+        obj_Data.set_data()
+        obj_Publisher.run(obj_Publisher.send_mqtt(obj_Data.get_data()))
 
 
 if __name__ == '__main__':
-    formatter = "[%(asctime)s] %(name)s {%(filename)s:%(lineno)d} %(levelname)s - %(message)s"
-    logging.basicConfig(level=logging.DEBUG, format=formatter)
-    asyncio.get_event_loop().run_until_complete(send_mqtt())
-    
+    main()
