52542383b95e254e9116ba7849d6755d5fc437d9
Marcos Carvalho <marcosf40@hotmail.com>
2018-05-27 19:35:39 -0300

    Create argparse

--- Publisher/publisher.py
+++ Publisher/publisher.py
@@ -4,7 +4,7 @@ import random
 import time
 from hbmqtt.client import MQTTClient
 from hbmqtt.mqtt.constants import QOS_1, QOS_2
-
+import argparse
 
 class Data(object):
     """This class is to gerate data.
@@ -44,8 +44,9 @@ class Publisher(object):
 
     """
 
-    def __init__(self):
+    def __init__(self, args):
         """ This constructor for Publisher """
+        self.arg = args
         try:
             self.client = MQTTClient()
         except Exception as e:
@@ -61,9 +62,8 @@ class Publisher(object):
         self.dataToSend = data
 
         try:
-            yield from self.client.connect('mqtt://192.168.0.16')
-            tasks = [asyncio.ensure_future(self.client.publish(
-                'sensor/temperature', bytes(str(self.dataToSend), 'utf-8')))]
+            yield from self.client.connect('mqtt://' + str(self.arg.server_ip))            
+            tasks = [asyncio.ensure_future(self.client.publish(self.arg.topic, bytes(str(self.dataToSend), 'utf-8')))]
 
             yield from asyncio.wait(tasks)
 
@@ -87,8 +87,18 @@ def main():
                     obj_Data (Object): An object of class Data.
                     obj_Publisher (Object): An object of class Publisher
     """
+
+    parser = argparse.ArgumentParser(
+        description='Subscribe to MQTT server')
+
+    parser.add_argument('--server_ip', type=str, required=False,
+                        default='127.0.0.1', help='IP of MQQT server')
+
+    parser.add_argument('--topic', type=str, required=False,
+                        default='sensor/temperature', help='Topic filters to subscribe.')
+
     obj_Data = Data()
-    obj_Publisher = Publisher()
+    obj_Publisher = Publisher(parser.parse_args())
 
     while True:
         obj_Data.set_data()
